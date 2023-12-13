04f2cef77b5b34ad7c61b01eb4a5c35ff71ac541
Administrator <Administrator@172.31.5.222>
2023-12-13 08:03:05 +0000

    new one

---
+++ src/test/java/testScripts/ChromeDevTools.java
@@ -0,0 +1,77 @@
+package testScripts;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v117.network.model.RequestWillBeSent;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.devtools.v117.network.Network;
+
+
+public class ChromeDevTools {
+	
+	ChromiumDriver driver;
+	DevTools devTools;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		driver=new ChromeDriver();
+		devTools=driver.getDevTools();
+		devTools.createSession();
+	}
+ // @Test
+  public void deviceModeTest() {
+	  
+	  Map deviceMetrics=new HashedMap() {{
+		  put("width",500);
+		  put("height",800);
+		  put("deviceScaleFactor",50);
+		  put("mobile",true);
+	  }};
+	  driver.executeCdpCommand("Emulation.setDeviceMetricsOverride", deviceMetrics);
+	  driver.get("https://www.selenium.dev/");
+  }
+  
+  //@Test
+  public void geoLocationTest()
+  {
+	  Map deviceMetrics=new HashedMap() {{
+		  put("latitude",41.331989);
+		  put("longitude",-74.356819);
+		  put("accuracy",100);
+	  }};
+	  
+	  driver.executeCdpCommand("Emulation.setGeolocationOverride", deviceMetrics);
+	  driver.get("https://oldnavy.gap.com/stores");
+  }
+@Test  
+  public void captureNWTrafficTest()
+  {
+	  Map deviceMetrics=new HashedMap() {{
+		  put("maxTotalBufferSize",1000);
+		  put("maxResourceBufferSize",1000);
+		  put("maxPostDataSize",1000);
+	  }};
+	  
+	  devTools.send(Network.enable(Optional.empty(),Optional.empty(),Optional.empty()));
+	  //driver.executeCdpCommand("Network.enable", deviceMetrics);
+	  devTools.addListener(Network.requestWillBeSent(),new Consumer<RequestWillBeSent>() {
+		          public void accept(RequestWillBeSent entry)
+		          {
+		        	  System.out.println("Request URI: "+entry.getRequest().getUrl()+"\n"
+		             +"With method: "+entry.getRequest().getMethod()+"\n");
+		        	  entry.getRequest().getMethod();
+		          }
+			});
+	  
+	  driver.get("https://www.selenium.dev/");
+	  devTools.send(Network.disable());
+  }
+}
---
+++ src/test/java/testScripts/RemoteDriverSample.java
@@ -0,0 +1,30 @@
+package testScripts;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class RemoteDriverSample {
+  @Test
+  public void sampleTest() throws MalformedURLException {
+	  
+	  ChromeOptions options=new ChromeOptions();
+	  options.setCapability(CapabilityType.PLATFORM_NAME, Platform.WINDOWS);
+	  String strHub="";
+	  RemoteWebDriver driver=new RemoteWebDriver(new URL(strHub),options);
+	  
+	  driver.get("https://www.google.com/");
+	  WebElement searchBox=driver.findElement(By.id("APjFqb"));
+	  searchBox.sendKeys("Selenium Tutorial");
+	  searchBox.submit();
+	  System.out.println(driver.getTitle());
+	  
+  }
+}
