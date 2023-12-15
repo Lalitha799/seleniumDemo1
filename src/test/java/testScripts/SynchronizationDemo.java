6a81c078e94b29c0a98a95f541faf5f7caeccc22
Administrator <Administrator@172.31.2.209>
2023-12-15 07:55:34 +0000

    sync commit

---
+++ src/test/java/testScripts/SynchronizationDemo.java
@@ -0,0 +1,28 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Assert;
+
+public class SynchronizationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//HTML tooltip
+//		final String name="Mail";
+//		WebDriver driver=new ChromeDriver();
+//		driver.get("https://demo.guru99.com/test/social-icon.html");
+//		WebElement tooltip = driver.findElement(By.xpath("//a[@class='fa fa-lg fa-envelope']"));
+//		String toolText = tooltip.getAttribute("title");
+//		Assert.assertEquals(toolText, name);
+//		System.out.println(toolText);
+		
+		
+      // Jquery tooltip
+		
+	}
+
+}
